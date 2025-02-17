b53bdfd6b30321e4974827160ac0d85d62bc5521
PrathameshWalunj <prathameshwalunj27@gmail.com>
2025-02-17 04:13:30 -0500

    Include directives, mqtt handler const/dest

--- src/core/mqtt_handler.cpp
+++ src/core/mqtt_handler.cpp
@@ -1,1 +1,16 @@
   
+#include "core/mqtt_handler.hpp"
+#include <iostream>
+#include <boost/bind/bind.hpp>
+
+namespace mitmqtt {
+
+MQTTHandler::MQTTHandler(boost::asio::io_context& ioc)
+    : ioc_(ioc)
+    , acceptor_(ioc)
+    , running_(false) {
+}
+
+MQTTHandler::~MQTTHandler() {
+    stop();
+}
