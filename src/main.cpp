260d3aa0e604eb37aa5d96ff80efbdf5d9ec58ee
PrathameshWalunj <prathameshwalunj27@gmail.com>
2025-02-16 23:48:19 -0500

    Create GLFW window with custom deleter

--- src/main.cpp
+++ src/main.cpp
@@ -4,3 +4,9 @@ private:
         if (!glfwInit()) {
             throw std::runtime_error("Failed to initialize GLFW");
         }
+    void createWindow() {
+        // create main application window
+        window.reset(glfwCreateWindow(1280, 720, "MITMqtt - MQTT Intercepting Proxy", nullptr, nullptr));
+        if (!window) {
+            throw std::runtime_error("Failed to create GLFW window");
+        }
