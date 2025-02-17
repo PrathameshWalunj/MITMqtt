db3ec21be7f789fe2852c0501be1b38b4b806d63
PrathameshWalunj <prathameshwalunj27@gmail.com>
2025-02-16 23:48:56 -0500

    Set up GLFW window hints for OpenGL version

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,25 @@ private:
         if (!glfwInit()) {
             throw std::runtime_error("Failed to initialize GLFW");
         }
+        // decide GL+GLSL versions
+        #if defined(IMGUI_IMPL_OPENGL_ES2)
+            const char* glsl_version = "#version 100";
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+            glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+        #elif defined(__APPLE__)
+            const char* glsl_version = "#version 150";
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+            glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+            glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        #else
+            const char* glsl_version = "#version 130";
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+        #endif
+        glsl_version_ = glsl_version;
+    }
     void createWindow() {
         // create main application window
         window.reset(glfwCreateWindow(1280, 720, "MITMqtt - MQTT Intercepting Proxy", nullptr, nullptr));
