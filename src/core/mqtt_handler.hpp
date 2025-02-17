5b6f6506046445bcdc319b219cff64d566537ee4
PrathameshWalunj <prathameshwalunj27@gmail.com>
2025-02-17 00:34:12 -0500

    create initial header file with Boost, standard includes and pragma once

--- src/core/mqtt_handler.hpp
+++ src/core/mqtt_handler.hpp
@@ -1,1 +1,13 @@
   
+#pragma once
+
+#include <boost/asio.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <memory>
+#include <string>
+#include <vector>
+#include <queue>
+#include <functional>
+
+namespace mitmqtt {
