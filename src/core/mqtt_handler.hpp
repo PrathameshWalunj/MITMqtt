d27ed8841dc559364d1401130811cfc0551b5544
PrathameshWalunj <prathameshwalunj27@gmail.com>
2025-02-17 00:39:11 -0500

    Define enum for mqtt packet types

--- src/core/mqtt_handler.hpp
+++ src/core/mqtt_handler.hpp
@@ -18,3 +18,20 @@ class MQTTConnection;
 // Callback types
 using PacketCallback = std::function<void(const MQTTPacket&)>;
 using ConnectionCallback = std::function<void(std::shared_ptr<MQTTConnection>)>;
+// MQTT packet types
+enum class PacketType : uint8_t {
+    CONNECT = 1,
+    CONNACK = 2,
+    PUBLISH = 3,
+    PUBACK = 4,
+    PUBREC = 5,
+    PUBREL = 6,
+    PUBCOMP = 7,
+    SUBSCRIBE = 8,
+    SUBACK = 9,
+    UNSUBSCRIBE = 10,
+    UNSUBACK = 11,
+    PINGREQ = 12,
+    PINGRESP = 13,
+    DISCONNECT = 14
+};
